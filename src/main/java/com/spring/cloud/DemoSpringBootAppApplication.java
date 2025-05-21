62212ca68734e8ee58b1d5e97e8fb1b3b800ad1d
shawbirendra <krs.birendra@gmail.com>
2025-05-21 13:06:25 +0530

    "added all 3 endpoints, test using postman"

---
+++ src/main/java/com/spring/cloud/DemoSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootAppApplication.class, args);
+	}
+
+}
